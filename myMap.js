3b9defed182886b43e847ee460c2cd2446c0fac0
chenchen2018 <33188805+chenchen2018@users.noreply.github.com>
2017-10-29 12:34:17 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,9 +1,34 @@
+var map_manager = {
+    "map": null,
+    "map_items": []
+}
+
+map_manager.map_items = [
+    {
+        "pokemon_id": 12,
+        "expire": 1509294548,
+        "longitude": -73.45,
+        "latitude": 40.75
+    },
+    {
+        "pokemon_id": 2,
+        "expire": 1509294548,
+        "longitude": -73.46,
+        "latitude": 40.75
+    }
+]
+
 function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'Apho_fBsREzpr_3XF00IG0TFAznxtp2mTE_6asHkHJTsylYXrt0lmavrW8ED0dvl'
     });
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), 
-                                             { icon: 'https://chenchen2018.github.io/pokemongo-frontend/pushpins/1.png',
-        });
-    map.entities.push(pushpin);
+    map_manager.map = map;
+    
+    for (var i in map_manager.map_items) {
+        var map_item = map_manager.map_items[i];
+        var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), 
+                                         { icon: 'https://chenchen2018.github.io/pokemongo-frontend/pushpins/' + map_item["pokemon_id"] + '.png',
+    });
+        map.entities.push(pushpin);
+    }
 }
