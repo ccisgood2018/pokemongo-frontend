50b9731d7f5458bdbd12325d9a86263498e7e8d4
ccisgood2018 <33188805+ccisgood2018@users.noreply.github.com>
2017-10-28 22:04:58 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,6 +1,6 @@
-var map;
-function loadMapScenario() {
-    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
-        credentials: 'Apho_fBsREzpr_3XF00IG0TFAznxtp2mTE_6asHkHJTsylYXrt0lmavrW8ED0dvl'
-    });
-}
+var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+    credentials: 'Apho_fBsREzpr_3XF00IG0TFAznxtp2mTE_6asHkHJTsylYXrt0lmavrW8ED0dvl'
+});
+var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://www.bingmapsportal.com/Content/images/poi_custom.png',
+    anchor: new Microsoft.Maps.Point(12, 39) });
+map.entities.push(pushpin);
