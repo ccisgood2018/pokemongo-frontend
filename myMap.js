6ee2f62ec9ad91bdd3297d22c7930ac36633d1a5
chenchen2018 <33188805+chenchen2018@users.noreply.github.com>
2017-10-29 14:41:38 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -32,7 +32,13 @@ function loadMapScenario() {
         credentials: 'Apho_fBsREzpr_3XF00IG0TFAznxtp2mTE_6asHkHJTsylYXrt0lmavrW8ED0dvl'
     });
     map_manager.map = map;
-    
+    refresh_pokemon();
+}
+
+function refresh_pokemon() {
+    // 1. prepare pushpins
+    var layer = new Microsoft.Maps.Layer();
+    var pushpins = [];
     for (var i in map_manager.map_items) {
         var map_item = map_manager.map_items[i];
         var icon_url = 'https://chenchen2018.github.io/pokemongo-frontend/pushpins/' + map_item["pokemon_id"] + '.png';
@@ -40,6 +46,12 @@ function loadMapScenario() {
         var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(latitude = map_item["latitude"], longitude = map_item["longitude"]),
                                          {title: count_down, 
                                           icon: icon_url});
-        map.entities.push(pushpin);
+        pushpins.push(pushpin);
     }
+    layer.add(pushpins);
+    
+    // 2. remove old pushpins
+    map_manager.map.layers.clear();
+    // 3. add new pushpins
+    map_manager.map.insert(layer);
 }
