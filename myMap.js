a137d6b8b01820a70493683e6ee87580c6042c06
chenchen2018 <33188805+chenchen2018@users.noreply.github.com>
2017-10-28 23:32:25 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -2,7 +2,7 @@ function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'Apho_fBsREzpr_3XF00IG0TFAznxtp2mTE_6asHkHJTsylYXrt0lmavrW8ED0dvl'
     });
-    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'pushpins/1.png',
+    var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), { icon: 'https://github.com/chenchen2018/pokemongo-frontend/tree/master/pushpins',
         anchor: new Microsoft.Maps.Point(12, 39) });
     map.entities.push(pushpin);
 }
