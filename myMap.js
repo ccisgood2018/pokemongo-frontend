0d7b405f9400ae7c0d6d9492a8af93255610bee8
ccisgood2018 <33188805+ccisgood2018@users.noreply.github.com>
2017-10-28 21:41:45 -0400

    Create myMap.js

---
+++ myMap.js
@@ -0,0 +1,6 @@
+var map;
+function loadMapScenario() {
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+        credentials: 'Apho_fBsREzpr_3XF00IG0TFAznxtp2mTE_6asHkHJTsylYXrt0lmavrW8ED0dvl'
+    });
+}
