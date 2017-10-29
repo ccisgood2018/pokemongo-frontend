f4b1a66bfeb906c5483a677ebe5b7ec7d4ee68a4
chenchen2018 <33188805+chenchen2018@users.noreply.github.com>
2017-10-29 14:31:17 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -18,6 +18,15 @@ map_manager.map_items = [
     }
 ]
 
+function get_count_down_from_timestamp(exipre) {
+    var now_time = new Date().getTime() / 1000;
+    var time_left = expire - now_time;
+    var second = Math.floor(time_left % 60);
+    var minute = Math.floor(time_left / 60);
+    return minute + ":" + second;
+}
+    
+
 function loadMapScenario() {
     var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'Apho_fBsREzpr_3XF00IG0TFAznxtp2mTE_6asHkHJTsylYXrt0lmavrW8ED0dvl'
@@ -26,8 +35,11 @@ function loadMapScenario() {
     
     for (var i in map_manager.map_items) {
         var map_item = map_manager.map_items[i];
+        var icon_url = 'https://chenchen2018.github.io/pokemongo-frontend/pushpins/' + map_item["pokemon_id"] + '.png';
+        var count_down = get_count_down_from_timestamp(map_item["expire"]);
         var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(latitude = map_item["latitude"], longitude = map_item["longitude"]),
-                                         { icon: 'https://chenchen2018.github.io/pokemongo-frontend/pushpins/' + map_item["pokemon_id"] + '.png'});
+                                         {title: count_down, 
+                                          icon: icon_url});
         map.entities.push(pushpin);
     }
 }
