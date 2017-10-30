de81d4f47b5cac5067e9b67aafbf1fb7eece6e90
chenchen2018 <33188805+chenchen2018@users.noreply.github.com>
2017-10-29 21:25:07 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -1,61 +1,87 @@
 var map_manager = {
-    "map": null,
-    "map_items": []
+    "map" : null,
+    "map_items" : []
 }
 
-function query_pokemon_data() {
-    var bounds = map_manager.map.getBounds();
-    var apigClient = apigClientFactory.newClient();
-    var params = {
-        north: bounds.getNorth(),
-        south: bounds.getSouth(),
-        east: bounds.getEast(),
-        west: bounds.getWest()
-    };
-    apigClient.mapPokemonGet(params, {}, {})
-        .then(function(result){
-            map_manager.map_items = result.data;
-        }).catch( function(result){
-            console.log(result);
-        });
-}
-
-function get_count_down_from_timestamp(expire) {
-    var now_time = new Date().getTime() / 1000;
-    var time_left = expire - now_time;
-    var second = Math.floor(time_left % 60);
-    var minute = Math.floor(time_left / 60);
-    return minute + ":" + second;
-}
-    
+map_manager.map_items = [
+    {
+      "pokemon_id" : 12,
+      "expire" : 1476589403,
+      "longitude" : -73.9800345,
+      "latitude" : 40.7596651,
+    }
+]
 
 function loadMapScenario() {
-    var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+    map_manager.map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
         credentials: 'Apho_fBsREzpr_3XF00IG0TFAznxtp2mTE_6asHkHJTsylYXrt0lmavrW8ED0dvl'
     });
-    map_manager.map = map;
-    query_pokemon_data();
-    window.setInterval(query_pokemon_data, 1000);
-    window.setInterval(refresh_pokemon, 1000);
+    add_pokemon_layer();
 }
 
-function refresh_pokemon() {
-    // 1. prepare pushpins
+// 1. Define pokemon data format, create mock pokemon data
+function get_counter_down_time_from_expire_epoch(epoch) {
+  var now_time = new Date().getTime() / 1000;
+  var time_left = epoch / 1000 - now_time;   // unit: second
+  var second = Math.floor(time_left % 60);
+  var minute = Math.floor(time_left / 60);
+  return minute + ":" + second;
+}
+
+// 2. Create pokemon image on map
+function get_pokemon_layer_from_map_items(map_items) {
     var layer = new Microsoft.Maps.Layer();
-    var pushpins = [];
-    for (var i in map_manager.map_items) {
-        var map_item = map_manager.map_items[i];
-        var icon_url = 'https://chenchen2018.github.io/pokemongo-frontend/pushpins/' + map_item["pokemon_id"] + '.png';
-        var count_down = get_count_down_from_timestamp(map_item["expire"]);
-        var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(latitude = map_item["latitude"], longitude = map_item["longitude"]),
-                                         {title: count_down, 
-                                          icon: icon_url});
-        pushpins.push(pushpin);
+    var pushpins = []
+    for (var i in map_items) {
+      var map_item = map_items[i];
+      var pushpin = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(map_item["latitude"], map_item["longitude"]), 
+                                               { icon: 'https://chenchen2018.github.io/pokemongo-frontend/pushpins/' + map_item["pokemon_id"] + '.png' ,
+                                                 title: get_counter_down_time_from_expire_epoch(map_item['expire']) });
+      pushpins.push(pushpin)
     }
     layer.add(pushpins);
-    
-    // 2. remove old pushpins
-    map_manager.map.layers.clear();
-    // 3. add new pushpins
-    map_manager.map.layers.insert(layer);
+    return layer;
 }
+function add_pokemon_layer() {
+    var pokemon_layer = get_pokemon_layer_from_map_items(map_manager.map_items)
+    map_manager.map.layers.insert(pokemon_layer);
+}
+
+// 3. Add pokemon counter down refresh.
+function refresh_pokemon_layer() {
+  // Prepare new layer
+  var pokemon_layer = get_pokemon_layer_from_map_items(map_manager.map_items)
+  // Remove old layer
+  map_manager.map.layers.clear()
+  // Add new layer
+  map_manager.map.layers.insert(pokemon_layer);
+}
+
+// 4. Connect with REST API
+function refresh_pokemon_data() {
+  // Get boundary of current map view
+  var bounds = map_manager.map.getBounds();
+  
+  // Request pokemons in current map view
+  var apigClient = apigClientFactory.newClient();
+  var params = {
+    north: bounds.getNorth(),
+    south: bounds.getSouth(),
+    west: bounds.getWest(),
+    east: bounds.getEast(),
+  };
+  var body = { };
+  var additionalParams = { };
+ 
+  apigClient.mapPokemonsGet(params, body, additionalParams)
+    .then(function(result){
+        //This is where you would put a success callback
+        map_manager.map_items = result.data;
+    }).catch( function(result){
+        //This is where you would put an error callback
+        console.log(result)
+    });   
+}
+window.setInterval(refresh_pokemon_data, 1000);
+
+window.setInterval(refresh_pokemon_layer, 1000);
