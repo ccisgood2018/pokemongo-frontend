6fb383164e04cf10668e40220c17bbbbb146d405
chenchen2018 <33188805+chenchen2018@users.noreply.github.com>
2017-10-29 21:06:24 -0400

    Update myMap.js

--- myMap.js
+++ myMap.js
@@ -15,7 +15,6 @@ function query_pokemon_data() {
     apigClient.mapPokemonGet(params, {}, {})
         .then(function(result){
             map_manager.map_items = result.data;
-            console.log(map_manager.map_items);
         }).catch( function(result){
             console.log(result);
         });
